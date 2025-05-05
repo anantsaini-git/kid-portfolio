142686008a11e668913bd937dcb636ed02a747c5
Anant Saini <as00767567@techmahindra.com>
2025-05-05 15:54:53 +0530

    Issue in hompage and domain mappings

--- src/App.js
+++ src/App.js
@@ -1,42 +1,23 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.js";
 import About from "./pages/About.js";
 import Gallery from "./pages/Gallery.js";
 import AcademicPerformance from './pages/AcademicPerformance.js';
 import Navbar from './common/Navbar.js';
 
-
 function App() {
-  /* return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  ); */
-  return (
-    <Router>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/academic" element={<AcademicPerformance />} />  
-      </Routes>
-    </Router>
-  );
+    return (
+        <BrowserRouter basename="/kid-portfolio">
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/gallery" element={<Gallery />} />
+                <Route path="/academic" element={<AcademicPerformance />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
